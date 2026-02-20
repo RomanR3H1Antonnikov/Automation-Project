d3bf0a4cd5a2ab715c1c09b1da25e38d7b6716aa
Роман <roman.v.antonnikov@gmail.com>
2026-02-20 16:35:52 +0300

    Выполнено ДЗ: Добавления Логирования

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -1,5 +1,7 @@
 import requests
 
+from utils.logger import Logger
+
 
 class HTTPMethods:
     headers = {'Content-type' : 'application/json'}
@@ -7,20 +9,28 @@ class HTTPMethods:
 
     @staticmethod
     def get(url):
+        Logger.add_request(url, method='GET')
         result = requests.get(url, headers=HTTPMethods.headers, cookies=HTTPMethods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def post(url, body):
+        Logger.add_request(url, method='POST')
         result = requests.post(url, json=body, headers=HTTPMethods.headers, cookies=HTTPMethods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def put(url, body):
+        Logger.add_request(url, method='PUT')
         result = requests.put(url, json=body, headers=HTTPMethods.headers, cookies=HTTPMethods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def delete(url, body):
+        Logger.add_request(url, method='DELETE')
         result = requests.delete(url, json=body, headers=HTTPMethods.headers, cookies=HTTPMethods.cookie)
+        Logger.add_response(result)
         return result
